5fd346a79d7cf1f7923a86d956bd8c06d1ef5230
повелитель <karakozova44d@mail.ru>
2023-02-15 10:57:33 +0300

    add Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int deposit = 1001;
+        int account = 999 + deposit;
+        int bonus = deposit / 100;
+        if (deposit < 1000) {
+            bonus = 0;
+        }
+        System.out.println("Итоговый счёт:" + account);
+        System.out.println("Бонусные рубли:" + bonus);
+    }
 }
